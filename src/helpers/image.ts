cba016b1e290df73911ed2065276175db48b0fc4
William Darian <williamdariansutedjo@gmail.com>
2024-09-07 00:59:37 +0900

    feat: image link

--- src/helpers/image.ts
+++ src/helpers/image.ts
@@ -1,3 +1,9 @@
+import { blurhashToCssGradientString } from "@unpic/placeholder";
+
+export function blurhash(data: string): { background: string } {
+  return { background: blurhashToCssGradientString(data) };
+}
+
 export function aspect(data: { height: number; width: number }): number {
   return data.width / data.height;
 }
